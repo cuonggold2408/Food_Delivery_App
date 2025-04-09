e6c70aad8c132a983eb942ff62fa4c4bdddc8bd4
cuonggold2408 <cuonggold2408@gmail.com>
2025-04-10 04:04:34 +0700

    hoan thien login,register,otp,logout

---
+++ backend/src/shared/filters/http-exception.filter.ts
@@ -0,0 +1,21 @@
+import { Logger, Catch, ArgumentsHost, HttpException } from '@nestjs/common';
+import { BaseExceptionFilter } from '@nestjs/core';
+import { ZodSerializationException } from 'nestjs-zod';
+
+@Catch(HttpException)
+export class HttpExceptionFilter extends BaseExceptionFilter {
+  private readonly logger = new Logger(HttpExceptionFilter.name);
+
+  catch(exception: HttpException, host: ArgumentsHost) {
+    if (exception instanceof ZodSerializationException) {
+      const zodError = exception.getZodError();
+      this.logger.error(
+        `ZodSerializationException: ${JSON.stringify(zodError.errors)}`,
+      );
+      const ctx = host.switchToHttp();
+      const response = ctx.getResponse();
+      return response.status(500).json({ errors: zodError.errors });
+    }
+    super.catch(exception, host);
+  }
+}
--- backend/src/shared/services/token.service.ts
+++ backend/src/shared/services/token.service.ts
@@ -1,36 +1,79 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import envConfig from 'src/shared/config';
-import { TokenPayload } from 'src/shared/types/jwt.type';
+import {
+  AccessTokenPayload,
+  AccessTokenPayloadCreate,
+  RefreshTokenPayload,
+  RefreshTokenPayloadCreate,
+} from 'src/shared/types/jwt.type';
+import { v4 as uuidv4 } from 'uuid';
+import * as jwt from 'jsonwebtoken';
 
 @Injectable()
 export class TokenService {
   constructor(private readonly jwtService: JwtService) {}
-  signAccessToken(payload: { userId: number }) {
-    return this.jwtService.sign(payload, {
-      secret: envConfig.ACCESS_TOKEN_SECRET,
-      expiresIn: envConfig.ACCESS_TOKEN_EXPIRES_IN,
-      algorithm: 'HS256',
-    });
+  signAccessToken(payload: AccessTokenPayloadCreate) {
+    return this.jwtService.sign(
+      { ...payload, uuid: uuidv4() },
+      {
+        secret: envConfig.ACCESS_TOKEN_SECRET,
+        expiresIn: envConfig.ACCESS_TOKEN_EXPIRES_IN,
+        algorithm: 'HS256',
+      },
+    );
   }
 
-  signRefreshToken(payload: { userId: number }) {
-    return this.jwtService.sign(payload, {
-      secret: envConfig.REFRESH_TOKEN_SECRET,
-      expiresIn: envConfig.REFRESH_TOKEN_EXPIRES_IN,
-      algorithm: 'HS256',
-    });
+  signRefreshToken(payload: RefreshTokenPayloadCreate) {
+    return this.jwtService.sign(
+      { ...payload, uuid: uuidv4() },
+      {
+        secret: envConfig.REFRESH_TOKEN_SECRET,
+        expiresIn: envConfig.REFRESH_TOKEN_EXPIRES_IN,
+        algorithm: 'HS256',
+      },
+    );
   }
 
-  verifyAccessToken(token: string): Promise<TokenPayload> {
-    return this.jwtService.verifyAsync(token, {
-      secret: envConfig.ACCESS_TOKEN_SECRET,
-    });
+  async verifyAccessToken(token: string): Promise<AccessTokenPayload> {
+    try {
+      return await this.jwtService.verifyAsync(token, {
+        secret: envConfig.ACCESS_TOKEN_SECRET,
+      });
+    } catch (error) {
+      if (error instanceof jwt.JsonWebTokenError) {
+        throw new UnauthorizedException({
+          message: 'Access token không hợp lệ',
+          error: 'INVALID_TOKEN',
+        });
+      } else if (error instanceof jwt.TokenExpiredError) {
+        throw new UnauthorizedException({
+          message: 'Access token đã hết hạn',
+          error: 'TOKEN_EXPIRED',
+        });
+      }
+      throw error;
+    }
   }
 
-  verifyRefreshToken(token: string): Promise<TokenPayload> {
-    return this.jwtService.verifyAsync(token, {
-      secret: envConfig.REFRESH_TOKEN_SECRET,
-    });
+  async verifyRefreshToken(token: string): Promise<RefreshTokenPayload> {
+    try {
+      return await this.jwtService.verifyAsync(token, {
+        secret: envConfig.REFRESH_TOKEN_SECRET,
+      });
+    } catch (error) {
+      if (error instanceof jwt.JsonWebTokenError) {
+        throw new UnauthorizedException({
+          message: 'Refresh token không hợp lệ',
+          error: 'INVALID_REFRESH_TOKEN',
+        });
+      } else if (error instanceof jwt.TokenExpiredError) {
+        throw new UnauthorizedException({
+          message: 'Refresh token đã hết hạn',
+          error: 'REFRESH_TOKEN_EXPIRED',
+        });
+      }
+      throw error;
+    }
   }
 }
