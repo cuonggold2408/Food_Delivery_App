94130ad5eb65df5e5d5edf6f2c328f0cc78777d9
cuonggold2408 <cuonggold2408@gmail.com>
2025-04-11 05:41:38 +0700

    fix import trùng data

--- backend/src/routes/product/product.controller.ts
+++ backend/src/routes/product/product.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { ProductService } from 'src/routes/product/product.service';
+import { IsPublic } from 'src/shared/decorators/auth.decorator';
 
 @Controller('products')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
   @Get('/')
+  @IsPublic()
   getAllProducts(
     @Query('page') page: number = 1,
     @Query('limit') limit: number = 10,
