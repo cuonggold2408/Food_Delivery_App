88d3a8be02394a3d8e6c1d940fda21ffac183012
cuonggold2408 <cuonggold2408@gmail.com>
2025-05-12 05:19:22 +0700

    update add,update product to cart and get all cart of user

--- backend/src/routes/cart/cart.controller.ts
+++ backend/src/routes/cart/cart.controller.ts
@@ -1,27 +1,27 @@
-import { Body, Controller, Param, Patch, Post, Req } from '@nestjs/common';
-import { ApiOperation } from '@nestjs/swagger';
-import { CartBodyDTO, CartItemBodyDTO } from 'src/routes/cart/cart.dto';
+import { Body, Controller, Get, Post, Req } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
+import { CartBodyDTO } from 'src/routes/cart/cart.dto';
 import { CartService } from 'src/routes/cart/cart.service';
 
 @Controller('cart')
+@ApiBearerAuth()
 export class CartController {
   constructor(private readonly cartService: CartService) {}
 
+  @Get('/')
+  @ApiOperation({ summary: 'Lấy danh sách giỏ hàng của 1 user' })
+  async getCart(@Req() req: any) {
+    const user_id = req.user.user_id;
+    return this.cartService.getCart(user_id);
+  }
+
   @Post()
-  @ApiOperation({ summary: 'Thêm món ăn vào giỏ hàng' })
+  @ApiOperation({
+    summary:
+      'Thêm món ăn vào giỏ hàng hoặc cập nhật thông tin sản phẩm trong giỏ hàng',
+  })
   async addToCart(@Body() body: CartBodyDTO, @Req() req: any) {
     const user_id = req.user.user_id;
     return this.cartService.addToCart(body, user_id);
-  }
-
-  @Patch(':cartItemId')
-  @ApiOperation({ summary: 'Cập nhật thông tin món ăn trong giỏ hàng' })
-  async updateItemCart(
-    @Param('cartItemId') cartItemId: number,
-    @Body() body: CartItemBodyDTO,
-    @Req() req: any,
-  ) {
-    const user_id = req.user.user_id;
-    return this.cartService.updateItemCart(cartItemId, body, user_id);
   }
 }
