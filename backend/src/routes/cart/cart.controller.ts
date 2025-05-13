cb736ff77eec0143f635bb9be283ac4628cf7864
cuonggold2408 <cuonggold2408@gmail.com>
2025-05-14 01:28:21 +0700

    Cập nhật nhà hàng gần user bk 10km, api cart của 1 nhà hàng

--- backend/src/routes/cart/cart.controller.ts
+++ backend/src/routes/cart/cart.controller.ts
@@ -1,5 +1,5 @@
-import { Body, Controller, Get, Post, Req } from '@nestjs/common';
-import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
+import { Body, Controller, Get, Post, Query, Req } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation, ApiQuery } from '@nestjs/swagger';
 import { CartBodyDTO } from 'src/routes/cart/cart.dto';
 import { CartService } from 'src/routes/cart/cart.service';
 
@@ -9,10 +9,21 @@ export class CartController {
   constructor(private readonly cartService: CartService) {}
 
   @Get('/')
-  @ApiOperation({ summary: 'Lấy danh sách giỏ hàng của 1 user' })
-  async getCart(@Req() req: any) {
+  @ApiOperation({ summary: 'Lấy giỏ hàng trong 1 nhà hàng' })
+  @ApiQuery({
+    name: 'restaurantId',
+    description: 'ID của nhà hàng',
+  })
+  async getCart(@Req() req: any, @Query('restaurantId') restaurantId: string) {
     const user_id = req.user.user_id;
-    return this.cartService.getCart(user_id);
+    return this.cartService.getCart(user_id, restaurantId);
+  }
+
+  @Get('/all')
+  @ApiOperation({ summary: 'Lấy danh sách giỏ hàng của 1 user' })
+  async getCartOfUser(@Req() req: any) {
+    const user_id = req.user.user_id;
+    return this.cartService.getCartOfUser(user_id);
   }
 
   @Post()
