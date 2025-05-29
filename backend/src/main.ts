c004ee5da12f0bad13726da1597cdff60bc1fca3
cuonggold2408 <cuonggold2408@gmail.com>
2025-05-30 00:55:01 +0700

    add cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,6 +7,13 @@ import { WebSocketAdapter } from 'src/websockets/websocket.adapter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
   app.useGlobalInterceptors(new ResponseInterceptor());
 
   // config swagger
