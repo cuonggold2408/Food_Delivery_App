366f91d9ae4d62ec495245ab0f9d6bbb5604da69
cuonggold2408 <cuonggold2408@gmail.com>
2025-05-30 03:34:52 +0700

    fix pay-ment-apikey

--- backend/src/shared/guards/payment-api-key.guard.ts
+++ backend/src/shared/guards/payment-api-key.guard.ts
@@ -7,15 +7,11 @@ import {
 import envConfig from 'src/shared/config';
 
 @Injectable()
-export class PaymentApiKeyGuard implements CanActivate {
+export class PaymentAPIKeyGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
     const request = context.switchToHttp().getRequest();
-    const authHeader = request.headers['authorization'];
-    const paymentAPIKey = authHeader?.startsWith('Apikey ')
-      ? authHeader.split('Apikey ')[1]
-      : authHeader?.split(' ')[1];
-
-    if (paymentAPIKey !== envConfig.PAYMENT_API_KEY) {
+    const paymentApiKey = request.headers['authorization']?.split(' ')[1];
+    if (paymentApiKey !== envConfig.PAYMENT_API_KEY) {
       throw new UnauthorizedException();
     }
     return true;
